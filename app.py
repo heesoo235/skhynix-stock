31e7fa0979db5bd0b80218e841bf44029ced2687
hz6450 <59460811+hz6450@users.noreply.github.com>
2025-06-07 21:13:08 +0900

    about페이지 생성

--- app.py
+++ app.py
@@ -34,6 +34,10 @@ def get_db_connection():
 def landing():
     return render_template('landing.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/create/', methods=['GET'])
 def create_form():
     return render_template('create.html')
